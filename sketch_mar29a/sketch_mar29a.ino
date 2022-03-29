ce94bd9ae22d050e784a04f0f2c3504dd5112d02
Markutr <71882804+Markutr@users.noreply.github.com>
2022-03-29 11:20:09 +0200

    Create sketch_mar29a.ino

---
+++ sketch_mar29a/sketch_mar29a.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+Servo myServo;
+Servo myServo2;
+
+void setup() {
+  // put your setup code here, to run once:
+  myServo.attach(3);
+  myServo2.attach(4);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  myServo.write(10);
+  delay(1000);
+  myServo2.write(10);
+  delay(1000);
+  myServo.write(90);
+  delay(1000);
+  myServo2.write(90);
+  delay(1000);
+  myServo.write(170);
+  delay(1000);
+  myServo2.write(170);
+  delay(1000);
+}
